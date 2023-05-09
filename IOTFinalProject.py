c524440763219a991d5b50ec744f0555350c9eef
Baccast <jeremybl2004@gmail.com>
2023-05-08 22:37:58 -0400

    made changes

--- IOTFinalProject.py
+++ IOTFinalProject.py
@@ -1,79 +1,72 @@
+#!/usr/bin/env python
 import RPi.GPIO as GPIO
 import time
 
-# RGB LED pins
-PIN_R = 5
+# GPIO channel numbers for RGB LED pins
+PIN_R = 11
 PIN_G = 6
 PIN_B = 13
 
-# Traffic light pins
-LIGHT_GREEN_PIN = 15
-LIGHT_YELLOW_PIN = 16
-LIGHT_RED_PIN = 18
-
-# RGB LED colors
+# Colors in RGB format
 COLOR_RED = (255, 0, 0)
 COLOR_YELLOW = (255, 255, 0)
 COLOR_GREEN = (0, 255, 0)
 
-# Traffic light durations (in seconds)
-GREEN_DURATION = 5
-YELLOW_DURATION = 2
-RED_DURATION = 5
-
-# Initialize GPIO
+# Setup GPIO pins
 GPIO.setmode(GPIO.BOARD)
-
-# Set up RGB LED pins
 GPIO.setup(PIN_R, GPIO.OUT)
 GPIO.setup(PIN_G, GPIO.OUT)
 GPIO.setup(PIN_B, GPIO.OUT)
-GPIO.output(PIN_R, GPIO.HIGH)
-GPIO.output(PIN_G, GPIO.HIGH)
-GPIO.output(PIN_B, GPIO.HIGH)
 
-# Set up traffic light pins
-GPIO.setup(LIGHT_GREEN_PIN, GPIO.OUT)
-GPIO.setup(LIGHT_YELLOW_PIN, GPIO.OUT)
-GPIO.setup(LIGHT_RED_PIN, GPIO.OUT)
+# Initialize PWM instances for each pin
+p_R = GPIO.PWM(PIN_R, 2000)  # Frequency: 2KHz
+p_G = GPIO.PWM(PIN_G, 2000)
+p_B = GPIO.PWM(PIN_B, 2000)
 
-
-def set_rgb_color(color):
-    # Since LED is common anode, use inverted logic
-    GPIO.output(PIN_R, not color[0])
-    GPIO.output(PIN_G, not color[1])
-    GPIO.output(PIN_B, not color[2])
+# Start with all LEDs off
+p_R.start(0)
+p_G.start(0)
+p_B.start(0)
 
 
-def set_traffic_light(color):
-    GPIO.output(LIGHT_GREEN_PIN, GPIO.LOW)
-    GPIO.output(LIGHT_YELLOW_PIN, GPIO.LOW)
-    GPIO.output(LIGHT_RED_PIN, GPIO.LOW)
+def set_color(color):
+    # Set the RGB LED color
+    r, g, b = color
 
-    if color == 'green':
-        GPIO.output(LIGHT_GREEN_PIN, GPIO.HIGH)
-    elif color == 'yellow':
-        GPIO.output(LIGHT_YELLOW_PIN, GPIO.HIGH)
-    elif color == 'red':
-        GPIO.output(LIGHT_RED_PIN, GPIO.HIGH)
+    # Map color values from 0-255 to 0-100 (duty cycle)
+    r = map_value(r, 0, 255, 0, 100)
+    g = map_value(g, 0, 255, 0, 100)
+    b = map_value(b, 0, 255, 0, 100)
+
+    # Set duty cycle for each pin
+    p_R.ChangeDutyCycle(r)
+    p_G.ChangeDutyCycle(g)
+    p_B.ChangeDutyCycle(b)
+
+
+def map_value(x, in_min, in_max, out_min, out_max):
+    # Map a value from one range to another
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 
 try:
     while True:
-        # Set traffic light to green
-        set_traffic_light('green')
-        set_rgb_color(COLOR_GREEN)
-        time.sleep(GREEN_DURATION)
+        # Set the RGB LED color based on the road light state
+        road_light_state = input("Enter road light state (red/yellow/green): ")
+        if road_light_state == "red":
+            set_color(COLOR_RED)
+        elif road_light_state == "yellow":
+            set_color(COLOR_YELLOW)
+        elif road_light_state == "green":
+            set_color(COLOR_GREEN)
+        else:
+            print("Invalid road light state. Please enter red, yellow, or green.")
 
-        # Set traffic light to yellow
-        set_traffic_light('yellow')
-        set_rgb_color(COLOR_YELLOW)
-        time.sleep(YELLOW_DURATION)
-
-        # Set traffic light to red
-        set_traffic_light('red')
-        set_rgb_color(COLOR_RED)
-        time.sleep(RED_DURATION)
+        time.sleep(1)
 
 except KeyboardInterrupt:
+    # Cleanup GPIO and stop PWM
+    p_R.stop()
+    p_G.stop()
+    p_B.stop()
     GPIO.cleanup()
